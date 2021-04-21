3fe034eaba512c0d6466ffaae1e3bd3fc212e862
ossama hyane <78233494+ossama-hyane@users.noreply.github.com>
2021-04-21 18:04:58 +0000

    Create 3-calculate_accuracy.py

---
+++ 0x02-tensorflow/3-calculate_accuracy.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Accuracy with TensorFlow """
+import tensorflow as tf
+
+
+def calculate_accuracy(y, y_pred):
+    """
+    y is a placeholder for the labels of the input data
+    y_pred is a tensor containing the network’s predictions
+    Returns: a tensor containing the decimal accuracy of the prediction
+    """
+    accuracy = tf.equal(tf.argmax(y, 1), tf.argmax(y_pred, 1))
+    a = tf.reduce_mean(tf.cast(c_p, tf.float32))
+    return a
