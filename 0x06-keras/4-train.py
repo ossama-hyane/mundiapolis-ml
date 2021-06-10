a5654692a977a0eced327861480eccc287d4b9c2
ossama hyane <78233494+ossama-hyane@users.noreply.github.com>
2021-06-10 20:27:15 +0100

    Update 4-train.py

--- 0x06-keras/4-train.py
+++ 0x06-keras/4-train.py
@@ -1,1 +1,11 @@
+#!/usr/bin/env python3
+""" Training keras model using mini-batch gradient descent"""
+import tensorflow.keras as K
 
+
+def train_model(network, data, labels, batch_size, epochs,
+                verbose=True, shuffle=False):
+    """ Training keras model using mini-batch gradient descent"""
+    history = network.fit(data, labels, batch_size=batch_size, epochs=epochs,
+                          verbose=verbose, shuffle=shuffle)
+    return history
